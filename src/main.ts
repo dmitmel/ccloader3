72c4301fe1018fc7789a15a63df41e9f8b388103
2767mr <2767mr@gmx.at>
2020-06-09 15:57:07 +0200

    Reworked main

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,42 @@
 import * as modloader from './modloader.js';
-import { showDevTools, wait } from '../common/dist/utils.js';
+import * as utils from '../common/dist/utils.js';
 
-(async () => {
+main().catch((err) => console.error('Error in main: ', err));
+
+async function main(): Promise<void> {
+	if (redirect()) {
+		return;
+	}
+
+	const onloadPromise = getOnloadPromise();
+	await openDevTools();
+	await onloadPromise;
+
+	await modloader.boot();
+}
+
+function redirect(): boolean {
 	const env = window.process?.env as NodeJS.ProcessEnv | undefined;
-
 	const urlOverride = env?.CCLOADER_OVERRIDE_MAIN_URL;
 	if (urlOverride) {
 		delete env?.CCLOADER_OVERRIDE_MAIN_URL;
 		window.location.replace(urlOverride);
-		return;
+		return true;
 	}
 
-	const onloadPromise = new Promise((resolve) => window.addEventListener('load', () => resolve()));
+	return false;
+}
 
+function getOnloadPromise(): Promise<void> {
+	return new Promise((resolve) => window.addEventListener('load', () => resolve()));
+}
+
+async function openDevTools(): Promise<void> {
+	const env = window.process?.env as NodeJS.ProcessEnv | undefined;
 	if (env?.CCLOADER_OPEN_DEVTOOLS) {
 		const win = nw.Window.get();
-		await showDevTools();
+		await utils.showDevTools();
 		win.focus();
-		await wait(500);
+		await utils.wait(500);
 	}
-
-	await onloadPromise;
-
-	await modloader.boot();
-})();
+}
