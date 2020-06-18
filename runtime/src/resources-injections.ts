abca1dd00b0a3f00b943f19fa52b7f09e183f66c
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-06-18 19:29:09 +0300

    reimplement asset overriding for audio files

--- runtime/src/resources-injections.ts
+++ runtime/src/resources-injections.ts
@@ -1,6 +1,5 @@
-import * as impactModuleHooks from './impact-module-hooks.js';
-import { loadImagePatched, loadJSONPatched } from './resources.js';
 import { GAME_ASSETS_URL, MOD_PROTOCOL_PREFIX } from './resources.constants.js';
+import * as impactModuleHooks from './impact-module-hooks.js';
 
 $.ajaxSetup({
   beforeSend(_jqXhr: JQueryXHR, settings: JQueryAjaxSettings): boolean {
@@ -10,7 +9,7 @@ $.ajaxSetup({
     if (typeof url !== 'string') return true;
 
     if (!url.startsWith(MOD_PROTOCOL_PREFIX)) {
-      let parsedUrl = new URL(url, GAME_ASSETS_URL).href;
+      let parsedUrl = new URL(url, document.baseURI).href;
       if (!parsedUrl.startsWith(GAME_ASSETS_URL.href)) return true;
     }
 
@@ -18,7 +17,8 @@ $.ajaxSetup({
     delete settings.success;
     delete settings.error;
     delete settings.complete;
-    loadJSONPatched(url)
+    ccmod3.resources
+      .loadJSONPatched(url)
       .then(
         (data) => {
           if (success != null) success.call(context, data, 'hijacked', null!);
@@ -39,17 +39,19 @@ $.ajaxSetup({
 impactModuleHooks.add('impact.base.image', () => {
   ig.Image.inject({
     loadInternal(path) {
-      loadImagePatched(
-        ig.getFilePath(`${ig.root}${path}${ig.getCacheSuffix()}`),
-      ).then(
-        (img) => {
-          this.data = img;
-          this.onload();
-        },
-        (_err) => {
-          this.onerror();
-        },
-      );
+      ccmod3.resources
+        .loadImagePatched(
+          ig.getFilePath(`${ig.root}${path}${ig.getCacheSuffix()}`),
+        )
+        .then(
+          (img) => {
+            this.data = img;
+            this.onload();
+          },
+          (_err) => {
+            this.onerror();
+          },
+        );
     },
 
     // the default implementation appends a query with the current date in
@@ -59,7 +61,7 @@ impactModuleHooks.add('impact.base.image', () => {
     // implementation
     debugReload: true,
     reload() {
-      // I kinda gave up fixing `ImpactClass` here
+      // I kinda gave up fixing the `ImpactClass` here
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       ig.Loadable.prototype.reload.call(this as any);
     },
@@ -67,9 +69,36 @@ impactModuleHooks.add('impact.base.image', () => {
 });
 
 impactModuleHooks.add('impact.base.sound', () => {
+  function loadAudio<
+    Ret,
+    Args extends [string, ...unknown[]],
+    This extends { parent(this: This, ...args: Args): Ret }
+  >(this: This, ...args: Args): Ret {
+    let path = args[0];
+
+    let pathWithoutExt = path;
+    let lastDotIndex = pathWithoutExt.lastIndexOf('.');
+    if (lastDotIndex >= 0) {
+      pathWithoutExt = pathWithoutExt.slice(0, lastDotIndex);
+    }
+
+    let cacheSuffix = ig.getCacheSuffix();
+    let resolvedURL = ig.getFilePath(
+      `${ig.root}${pathWithoutExt}.${ig.soundManager.format.ext}${cacheSuffix}`,
+    );
+    resolvedURL = ccmod3.resources.resolveURL(resolvedURL);
+
+    let originalGetFilePath = ig.getFilePath;
+    try {
+      ig.getFilePath = (_path) => resolvedURL;
+      return this.parent(...args);
+    } finally {
+      ig.getFilePath = originalGetFilePath;
+    }
+  }
+
   ig.SoundManager.inject({
-    loadWebAudio(path, ...args) {
-      return this.parent(path, ...args);
-    },
+    loadWebAudio: loadAudio,
+    load: loadAudio,
   });
 });
