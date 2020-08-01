453f3cf59b4f4e1d106f7a20c154dd250e5c3b8f
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-08-01 20:37:46 +0300

    implement a very simple console, with feature-set analogous to that of v2

--- common/src/console.ts
+++ common/src/console.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+const nodejsUtil =
+  typeof require !== 'undefined' ? (require('util') as typeof import('util')) : null;
+
 enum LogLevel {
   LOG = 2,
   WARN = 1,
@@ -7,32 +12,27 @@ enum LogLevel {
 type LogLevelName = keyof typeof LogLevel;
 type LogLevelsDict<T = boolean> = Record<LogLevelName, T>;
 
-const LOG_LEVEL_MASKS: LogLevelsDict<number> = {
-  LOG: 1 << LogLevel.LOG,
-  WARN: 1 << LogLevel.WARN,
-  ERROR: 1 << LogLevel.ERROR,
-};
-
 export const DEFAULT_LOG_LEVELS: LogLevelsDict = {
   LOG: false,
   WARN: true,
   ERROR: true,
 };
 
-function logLevelsToBitFlags(logLevels: LogLevelsDict): number {
+function logLevelsToBitFlags(levels: LogLevelsDict): number {
   let flags = 0;
-  for (let [level, enabled] of Object.entries(logLevels)) {
+  for (let [level, enabled] of Object.entries(levels)) {
     flags |= Number(enabled) << LogLevel[level as LogLevelName];
   }
   return flags;
 }
 
 function logLevelsFromBitFlags(flags: number): LogLevelsDict {
-  let logLevels = {} as LogLevelsDict;
-  for (let [level, mask] of Object.entries(LOG_LEVEL_MASKS)) {
-    logLevels[level as LogLevelName] = Boolean(flags & mask);
+  let levels = {} as LogLevelsDict;
+  for (let [level, bit] of Object.entries(LogLevel)) {
+    if (typeof bit !== 'number') continue;
+    levels[level as LogLevelName] = Boolean(flags & (1 << bit));
   }
-  return logLevels;
+  return levels;
 }
 
 function getLogLevels(): LogLevelsDict {
@@ -46,11 +46,143 @@ function getLogLevels(): LogLevelsDict {
   return { ...DEFAULT_LOG_LEVELS };
 }
 
-export function setLogLevels(logLevels: LogLevelsDict): void {
-  localStorage.setItem('logFlags', String(logLevelsToBitFlags(logLevels)));
+export function setLogLevels(levels: LogLevelsDict): void {
+  localStorage.setItem('logFlags', String(logLevelsToBitFlags(levels)));
 }
 
+const EVENTS_BLOCKED_BY_CONSOLE: Array<keyof WindowEventMap> = [
+  'mousewheel',
+  'contextmenu',
+  'mousedown',
+  'mouseup',
+  'mousemove',
+  'touchstart',
+  'touchend',
+  'touchmove',
+  'keydown',
+  'keyup',
+  'keypress',
+];
+
+interface Color {
+  r: number;
+  g: number;
+  b: number;
+  a?: number | null;
+}
+
+namespace Color {
+  export function toCSS({ r, g, b, a }: Readonly<Color>): string {
+    return a != null ? `rgb(${r},${g},${b},${a})` : `rgb(${r},${g},${b})`;
+  }
+
+  export function rgb(r: number, g: number, b: number, a?: number | null): Color {
+    return { r, g, b, a };
+  }
+}
+
+import rgb = Color.rgb;
+
+// colors were taken from the material palette, see <https://material.io/resources/color/>
+const TEXT_COLOR = '#e0e0e0'; // Grey 300
+const LOG_LEVEL_COLORS: LogLevelsDict<{
+  readonly bg: Readonly<Color>;
+  readonly border: Readonly<Color>;
+}> = {
+  LOG: { bg: rgb(27, 27, 27) /* Grey 800 dark */, border: rgb(66, 66, 66) /* Grey 800 */ },
+  WARN: { bg: rgb(196, 62, 0) /* Amber 900 dark */, border: rgb(255, 111, 0) /* Amber 900 */ },
+  ERROR: { bg: rgb(127, 0, 0) /* Red 900 dark */, border: rgb(183, 28, 28) /* Red 900 */ },
+};
+
+const LOG_LEVEL_APPEARENCE_DURATIONS: LogLevelsDict<number> = {
+  LOG: 2000,
+  WARN: 5000,
+  ERROR: 15000,
+};
+
+/* eslint-disable no-multi-assign */
+
+let rootElement: HTMLElement;
 export function inject(): void {
   let logLevels = getLogLevels();
-  console.log(logLevels);
+
+  {
+    let el = document.createElement('pre');
+    rootElement = el;
+    el.id = 'console';
+    el.style.display = 'flex';
+    el.style.flexDirection = 'column';
+    el.style.position = 'fixed';
+    el.style.top = el.style.left = el.style.right = '0';
+    el.style.zIndex = '9999';
+    el.style.maxHeight = '100%';
+    el.style.fontSize = '18px';
+    el.style.whiteSpace = 'pre-wrap';
+    el.style.wordBreak = 'break-all';
+    el.style.overflowY = 'auto';
+
+    for (let eventType of EVENTS_BLOCKED_BY_CONSOLE) {
+      el.addEventListener(eventType, (event) => event.stopPropagation());
+    }
+
+    document.body.append(el);
+  }
+
+  function hookConsoleMethod(name: keyof typeof console, level: LogLevel): void {
+    let old = console[name] as (...message: unknown[]) => void;
+    console[name] = function (...message: unknown[]): void {
+      let result = old.apply(this, message);
+      log(level, ...message);
+      return result;
+    };
+  }
+
+  if (logLevels.ERROR) {
+    hookConsoleMethod('error', LogLevel.ERROR);
+  }
+  if (logLevels.WARN) {
+    hookConsoleMethod('warn', LogLevel.WARN);
+  }
+  if (logLevels.LOG) {
+    hookConsoleMethod('log', LogLevel.LOG);
+    hookConsoleMethod('info', LogLevel.LOG);
+  }
+}
+
+function log(level: LogLevel, ...message: unknown[]): void {
+  let levelName = LogLevel[level] as LogLevelName;
+
+  let el = document.createElement('code');
+  el.classList.add('message', levelName);
+  el.style.padding = '4px';
+  el.style.borderBottom = '2px solid';
+
+  el.style.color = TEXT_COLOR;
+  let colors = LOG_LEVEL_COLORS[levelName];
+  el.style.backgroundColor = Color.toCSS({ ...colors.bg, a: 0.9 });
+  el.style.borderColor = Color.toCSS(colors.border);
+
+  el.textContent = `[${LogLevel[level]}] ${formatMessage(...message)}`;
+
+  rootElement.append(el);
+
+  let removeTimeout = setTimeout(() => {
+    el.remove();
+  }, LOG_LEVEL_APPEARENCE_DURATIONS[levelName]);
+
+  el.addEventListener('click', (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    el.remove();
+    clearTimeout(removeTimeout);
+  });
+}
+
+function formatMessage(...message: unknown[]): string {
+  if (nodejsUtil != null) {
+    let formatFn = nodejsUtil.format as (...args: unknown[]) => string;
+    return formatFn(...message);
+  } else {
+    return message.map((m) => String(m)).join(' ');
+  }
 }
