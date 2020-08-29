fa1cd413484e40cee28321747735fc21580ab11d
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-08-29 23:03:05 +0300

    patch all standard Lang Files to support modded locales not based on en_US

--- runtime/src/lang-file-patcher.ts
+++ runtime/src/lang-file-patcher.ts
@@ -2,18 +2,21 @@ import * as impactModuleHooks from './impact-module-hooks.js';
 import * as resources from './resources.js';
 
 impactModuleHooks.add('game.config', () => {
-  let builtinLocales = Object.keys(ig.LANG_DETAILS);
+  const BUILTIN_LOCALES = Object.keys(ig.LANG_DETAILS);
+  const DEFAULT_LOCALE = 'en_US';
 
-  function getLangFilePath(feature: string, lang: string): string {
-    return feature.toPath('data/lang/', `.${lang}.json`);
+  function getLangFilePath(feature: string, locale: string): string {
+    return feature.toPath('data/lang/', `.${locale}.json`);
   }
 
   ig.Lang.inject({
     loadInternal(...args) {
-      if (ig.currentLang !== 'en_US' && builtinLocales.includes(ig.currentLang)) {
+      for (let locale of BUILTIN_LOCALES) {
+        if (locale === DEFAULT_LOCALE) continue;
+
         for (let feature of ig.langFileList) {
-          resources.jsonPatches.add(getLangFilePath(feature, ig.currentLang), {
-            dependencies: () => resources.loadJSON(getLangFilePath(feature, 'en_US')),
+          resources.jsonPatches.add(getLangFilePath(feature, locale), {
+            dependencies: () => resources.loadJSON(getLangFilePath(feature, DEFAULT_LOCALE)),
             // eslint-disable-next-line no-loop-func
             patcher: (data, englishData) =>
               ig.merge(/* original */ englishData, /* new */ data, /* noArrayMerge */ true),
