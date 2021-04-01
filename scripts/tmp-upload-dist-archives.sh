ce3a75488fd87eba9455f9fb4e7bdbad900898b6
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-04-01 19:07:38 +0300

    add a script for uploading development releases to the temporary server

---
+++ scripts/tmp-upload-dist-archives.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# Uploads distributable archives to a temporary server.
+
+set -eu
+
+# Aliases taken from my dotfiles:
+alias rsync-backup="rsync --archive --compress --verbose --human-readable --partial --progress"
+alias date-fmt-compact="date +%Y%m%d%H%M%S"
+
+date=$(date-fmt-compact --utc)
+
+rsync-backup ccloader_*_{quick-install,package}.{zip,tar.gz} experiments:public/ccloader3/"$date"/
+ssh experiments ln -svfT "$date" public/ccloader3/latest
